c709bbe34f2f98ddc3c70def6aed4b3c3b2d464a
Daniel Ferreira <daniel_jferreira@hotmail.com>
2023-01-31 13:51:29 +0000

    adding 5-day forecast fetch function for the date, ico, temperature and humidity  and render, attritubete funciot momment to date and looping to 5 day(issues)

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,62 +13,80 @@
  // Container element for 5-day forecast
  const fiveDayForecast = document.getElementById("forecast");
  
- // // Search history container
- // const searchHistory = document.querySelector(".list-group");
+ // Search history container
+ const searchHistory = document.getElementById("history");
  
  // Fetch current weather data for a city
- const fetchCurrentWeather = city => {
-   return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
-     .then(res => res.json())
-     .then(data => {
-       return {
-         cityName: data.name,
-         date: new Date(),
-         icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-         temperature: data.main.temp,
-         humidity: data.main.humidity,
-         windSpeed: data.wind.speed
-       };
-     });
+ const fetchCurrentWeather = async city => {
+   const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+   const data = await res.json();
+   return {
+     cityName: data.name,
+     date: new Date(),
+     icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+     temperature: Math.floor(data.main.temp - 32) * 5 / 9,
+     humidity: data.main.humidity,
+     windSpeed: data.wind.speed
+   };
  };
 
-  // Render current weather data to the page
+// Render current weather data to the page
   const renderCurrentWeather = data => {
     currentWeather.innerHTML = `
-    <h1>${data.cityName} (${data.date.toDateString()})</h1>
-      <img src="${data.icon}" alt="Weather icon">
-      <p>Temperature: ${data.temperature}°F</p>
+    <h1>${data.cityName} (${moment(data.date).format("DD MM YYYY")}) <img src="${data.icon}" alt="Weather icon"></h1>
+      <p>Temperature: ${data.temperature}°C</p>
       <p>Humidity: ${data.humidity}%</p>
-      <p>Wind Speed: ${data.windSpeed}mph</p>
+      <p>Wind Speed: ${Math.round(data.windSpeed)}mph</p>
     `;
   };
  
+// Fetch 5-day forecast for a city
+const fetchFiveDayForecast = async city => {
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`);
+  const data = await res.json();
+  return data.list.map(forecast => {
+    return {
+      date: new Date(forecast.dt * 1000),
+      icon: `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`,
+      temperature: forecast.main.temp,
+      humidity: forecast.main.humidity
+    };
+  });
+};
+
+ // Render 5-day forecast to the page 
+const renderFiveDayForecast = data => { 
+  fiveDayForecast.innerHTML = ""; 
+
+  for (let i = 0; i < 5; i++) {   // loop through the first 5 days of the forecast array  
+
+    const forecastEl = document.createElement("div");   // create a div element for each day's forecast
+
+    forecastEl.innerHTML = `      
+       <p>${moment(data[i].date).format("DD MM YYYY")}</p>       
+       <img src="${data[i].icon}" alt="Weather icon">     
+       <p>Temperature: ${data[i].temperature}°F</p>      
+       <p>Humidity: ${data[i].humidity}%</p>      `;
+
+    fiveDayForecast.appendChild(forecastEl);   // append each day's forecast to the fiveDayForecast container on the page 
+
+  }   // end of loop through first 5 days of forecast array   
+
+ };
+
        // Handle city search form submission
        const handleCitySearch = event => {
        event.preventDefault();
        const city = cityInput.value;
-       Promise.all([fetchCurrentWeather(city)])
-       .then(([currentWeatherData]) => {
+       Promise.all([fetchCurrentWeather(city), fetchFiveDayForecast(city)])
+       .then(([currentWeatherData, fiveDayForecastData]) => {
        renderCurrentWeather(currentWeatherData);
-      
+       renderFiveDayForecast(fiveDayForecastData);
        });
        cityInput.value = "";
        };
-       
-       // Add city to search history
-       const addToSearchHistory = city => {
-       const cityEl = document.querySelector(".list-group");
-       cityEl.innerHTML +=`<li> <a href="#" class="list-group-item list-group-item-action">${city}</a></li>`;
-       searchHistory.appendChild(cityEl);
-       };
-   
-             // Add the city to the search history
-         const history = document.getElementById("history");
-         history.innerHTML += `<a href="#" class="list-group-item list-group-item-action">${cityInput}</a>`;
-         
-         // Clear the input field
-         cityInput.value = "";
+
  
-       // Event listener for city search form submission
-       form.addEventListener("submit", handleCitySearch);
+// Event listener for city search form submission
+  form.addEventListener("submit", handleCitySearch);
  
